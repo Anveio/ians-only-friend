822f43539ddddd44cd3e11381c5784dc9d41fb03
Shovon Hasan <im.shovonhasan@gmail.com>
2017-04-04 21:06:45 +0000

    Add dependencies and make first version of bot

---
+++ commands/random/rollthebones.js
@@ -0,0 +1,43 @@
+const commando = require('discord.js-commando');
+
+class RollTheBonesCmd extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'roll',
+      group: 'random',
+      memberName: 'roll',
+      description: 'Allows users to simulate Roll the Bones ability in WoW'
+    });
+    
+    this.BUFFS_ARRAY = ["Grand Melee", "Jolly Roger", "Buried Treasure", "Broadsides", "Shark Infested Waters", "True Bearing"];
+  }
+  async run(message, args) {
+    var roll = 6;
+    // var roll = this._generateRoll();
+    message.reply("You rolled a " + roll);
+  }
+  
+  _rawRoll() {  
+    return this.BUFFS_ARRAY.map(element => 
+    this.BUFFS_ARRAY[Math.floor(Math.random() * this.BUFFS_ARRAY.length)]);
+  }
+  
+  _generateHistogram(ary) {  
+    return ary.reduce((counter, item) => {
+      let el = item;
+      counter[el] = counter.hasOwnProperty(el) ? counter[el] + 1 : 1;
+      return counter;
+    }, {});
+  }
+  
+  _findModes(histogram) {  
+    const mode = Math.max(...Object.values(histogram));
+    return Object.keys(histogram).filter(el => histogram[el] == mode);
+  }
+  
+  _generateRoll() { 
+    return (this._findModes(this._generateHistogram(this._rawRoll())));
+  }  
+}
+
+module.exports = RollTheBonesCmd;
---
+++ index.js
@@ -0,0 +1,20 @@
+// const Discord = require("discord.js");
+const Commando = require('discord.js-commando');
+const client = new Commando.Client();
+
+client.registry.registerGroup('random', "Random");
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + "/commands");
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('Mjk4OTE0ODI3NjEzNzY1NjMz.C8WRsw.LxivOYZp8m9ANtgBsmAYuvwKHbM');
